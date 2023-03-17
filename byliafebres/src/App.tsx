264f34d224955100499b02203f648d44631c52d3
Axel <axel_clemente@yahoo.com>
2023-03-17 18:00:35 +0100

    navbar in progress

--- byliafebres/src/App.tsx
+++ byliafebres/src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import Navbar from "@/scenes/navbar"
+import { SelectedPage } from '@/shared/types';
 
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
 
   return (
-    <div className="app bg-gray-20">app
-
+    <div className="app bg-gray-20">
+      <Navbar
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}
+      />
     </div>
   )
 }
