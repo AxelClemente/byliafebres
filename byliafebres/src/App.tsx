3acd5779159407f059f83005ca894a873171a7c9
Axel <axel_clemente@yahoo.com>
2023-03-17 12:06:26 +0100

    first commit

---
+++ byliafebres/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+  return (
+    <div className="app bg-gray-20">app
+
+    </div>
+  )
+}
+
+export default App
